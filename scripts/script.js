1f895747cf5efd48e818781063e6b26173844cbe
Sarah <shy_h@telus.net>
2024-12-29 00:52:54 -0800

    Reorganize into separate functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
-function get_weather()
-{
+function get_time()
+{   
     var city = document.getElementById("my_city").value;
-    const select_units = document.getElementById("units");  
-    let units = select_units.value;
 
     const api_key = 'b2273b21514c4decb2b45606240806';  // API key from weatherapi.com
-    const url = "https://api.weatherapi.com/v1/forecast.json?key="+api_key+"&q="+city+"&days=3"; 
-    
+    const url = "https://api.weatherapi.com/v1/forecast.json?key="+api_key+"&q="+city; 
+
     fetch(url)
     .then(function(response) {
         return response.json(); // Parse the response body as JSON
@@ -14,20 +12,8 @@ function get_weather()
     .then(function(data) {
         console.log(data);  // Log the data to check it in the console
 
-        // Display the data on the webpage using backticks for template literals
-        let temp_output = document.getElementById("tempurature_output");
-        let tomorrow_output = document.getElementById("tomorrow_output")
-        let precip_output = document.getElementById("precip_output");
-        let wind_output = document.getElementById("wind_output");
-        let update = document.getElementById("update_output");
-        let cond_icon = document.getElementById("cond_icon");
-
-        let nowTemp_output = document.getElementById("Nowtempurature_output")
-
-        // To be outputed 
         let loc = data.location.name+", "+data.location.region+", "+data.location.country;
         let cond = data.forecast.forecastday[0].day.condition.text+"<br>";
-        let icon = data.forecast.forecastday[0].day.condition.icon;
 
         // LOCAL TIME/DATE FUNCTION
         let timeLocalOutput = document.getElementById("local_time");
@@ -67,6 +53,38 @@ function get_weather()
             updateLocalTime();
             setInterval(updateLocalTime, 60000); // Refresh the time every 60 seconds (60000 milliseconds)
 
+    })
+}
+
+function get_today()
+{
+    var city = document.getElementById("my_city").value;
+    const select_units = document.getElementById("units");  
+    let units = select_units.value;
+
+    const api_key = 'b2273b21514c4decb2b45606240806';  // API key from weatherapi.com
+    const url = "https://api.weatherapi.com/v1/forecast.json?key="+api_key+"&q="+city+"&days=3"; 
+    
+    fetch(url)
+    .then(function(response) {
+        return response.json(); // Parse the response body as JSON
+    })
+    .then(function(data) {
+        console.log(data);  // Log the data to check it in the console
+
+        // Display the data on the webpage using backticks for template literals
+        let temp_output = document.getElementById("tempurature_output");
+        let precip_output = document.getElementById("precip_output");
+        let wind_output = document.getElementById("wind_output");
+        let update = document.getElementById("update_output");
+        let cond_icon = document.getElementById("cond_icon");
+
+        let nowTemp_output = document.getElementById("Nowtempurature_output")
+
+        // To be outputed 
+        let icon = data.forecast.forecastday[0].day.condition.icon;
+
+
         switch(units)
             {
             case 'i':
@@ -76,10 +94,6 @@ function get_weather()
                     + "High: "+data.forecast.forecastday[0].day.maxtemp_f+"°F <br>"
                     + "Low: "+data.forecast.forecastday[0].day.mintemp_f+"°F <br>"
                     + "Average: "+data.forecast.forecastday[0].day.avgtemp_f+"°F <br>");
-
-                tempTomorrow = ("<b>TOMORROW</b><br>High: "+data.forecast.forecastday[1].day.maxtemp_f+"°F <br>"
-                        + "Low: "+data.forecast.forecastday[1].day.mintemp_f+"°F <br>"
-                        + "Average: "+data.forecast.forecastday[1].day.avgtemp_f+"°F <br>");
 
                 precip = ("Precipication: "+data.current.precip_in+" in <br>"
                     + "Total Precipication: "+data.forecast.forecastday[0].day.totalprecip_in+" in <br>"
@@ -108,10 +122,6 @@ function get_weather()
                     + "Low: "+((273.15+data.forecast.forecastday[0].day.mintemp_c).toFixed(2))+"K <br>"
                     + "Average: "+((273.15+data.forecast.forecastday[0].day.avgtemp_c).toFixed(2))+"K <br>");
 
-                tempTomorrow = ("<b>TOMORROW</b><br>High: "+((273.15+data.forecast.forecastday[1].day.maxtemp_c).toFixed(2))+"K <br>"
-                    + "Low: "+((273.15+data.forecast.forecastday[1].day.mintemp_c).toFixed(2))+"K <br>"
-                    + "Average: "+((273.15+data.forecast.forecastday[1].day.avgtemp_c).toFixed(2))+"K <br>");
-
                 precip = ("Precipication: "+data.current.precip_mm+" mm <br>"
                     + "Total Precipication: "+data.forecast.forecastday[0].day.totalprecip_mm+" mm <br>"
                     + "Humitidy: "+data.current.humidity+"%<br>"
@@ -138,10 +148,6 @@ function get_weather()
                     + "Low: "+data.forecast.forecastday[0].day.mintemp_c+"°C <br>"
                     + "Average: "+data.forecast.forecastday[0].day.avgtemp_c+"°C <br>");
 
-                tempTomorrow = ("<b>TOMORROW</b><br>High: "+data.forecast.forecastday[1].day.maxtemp_c+"°C <br>"
-                    + "Low: "+data.forecast.forecastday[1].day.mintemp_c+"°C <br>"
-                    + "Average: "+data.forecast.forecastday[1].day.avgtemp_c+"°C <br>");
-
                 precip = ("Precipication: "+data.current.precip_mm+" mm <br>"
                     + "Total Precipication: "+data.forecast.forecastday[0].day.totalprecip_mm+" mm <br>"
                     + "Humitidy: "+data.current.humidity+"%<br>"
@@ -163,17 +169,75 @@ function get_weather()
             }
 
         //OUTPUTS
-        temp_output.innerHTML = temp; 
-        tomorrow_output.innerHTML = tempTomorrow
-
-        precip_output.innerHTML = `${precip}`;
-        wind_output.innerHTML =`${wind}`
+        temp_output.innerHTML = `${temp}<br>${precip}<br>${wind}`; 
         update.innerHTML = `Last Updated: ${data.current.last_updated} in ${data.location.tz_id} timezone<br>`;
         cond_icon.src = `${icon}`;
 
         nowTemp_output.innerHTML = `${nowTemp}`; 
     })
 
+}
+
+function get_tomorrow()
+{
+    var city = document.getElementById("my_city").value;
+    const select_units = document.getElementById("units");
+    let units = select_units.value;
+
+    const api_key = 'b2273b21514c4decb2b45606240806';  // API key from weatherapi.com
+    const url = "https://api.weatherapi.com/v1/forecast.json?key="+api_key+"&q="+city+"&days=3"; 
+
+    fetch(url)
+    .then(function(response) {
+        return response.json(); // Parse the response body as JSON
+    })
+    .then(function(data) {
+        console.log(data);  // Log the data to check it in the console
+
+        // Display the data on the webpage using backticks for template literals
+        let tomorrow_output = document.getElementById("tomorrow_output")
+        let tomicon = document.getElementById("tomicon");
+
+        // To be outputed 
+        let icon = data.forecast.forecastday[2].day.condition.icon;
+
+        // LOCAL TIME/DATE FUNCTION
+        const forecastDay = new Date(data.forecast.forecastday[1].date);
+        const afterDate = forecastDay.toLocaleDateString('en-US', {
+            weekday: 'short',
+        });
+        date = afterDate.toUpperCase();
+        
+        switch(units)
+            {
+            case 'i':
+                tempTomorrow = ("<b>"+date+"</b><br>High: "+data.forecast.forecastday[1].day.maxtemp_f+"°F <br>"
+                    + "Low: "+data.forecast.forecastday[1].day.mintemp_f+"°F <br>"
+                    + "Average: "+data.forecast.forecastday[1].day.avgtemp_f+"°F <br>");
+                break;
+
+            case 'k':
+                tempTomorrow = ("<b>"+date+"</b><br>High: "+((273.15+data.forecast.forecastday[1].day.maxtemp_c).toFixed(2))+"K <br>"
+                    + "Low: "+((273.15+ddata.forecast.forecastday[1].day.mintemp_c).toFixed(2))+"K <br>"
+                    + "Average: "+((273.15+ddata.forecast.forecastday[1].day.avgtemp_c).toFixed(2))+"K <br>");
+                break;
+
+            default:
+                tempTomorrow = ("<b>"+date+"</b><br>High: "+data.forecast.forecastday[1].day.maxtemp_c+"°C <br>"
+                    + "Low: "+data.forecast.forecastday[1].day.mintemp_c+"°C <br>"
+                    + "Average: "+data.forecast.forecastday[1].day.avgtemp_c+"°C <br>");
+                break;
+            }
+
+        //OUTPUTS
+        tomorrow_output.innerHTML = tempTomorrow;
+        tomicon.src = `${icon}`;
+    })
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+        let output = document.getElementById("tempurature_output");
+        output.innerHTML = "Error: Unable to fetch weather data."; // Display an error message
+    });
 }
 
 function get_after()
@@ -240,6 +304,8 @@ function get_after()
 
 function main()
 {
-    get_weather();
+    get_time();
+    get_today();
+    get_tomorrow();
     get_after();
 }
